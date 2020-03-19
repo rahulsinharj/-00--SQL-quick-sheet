a06f924348255799b0f5918690de678e8b434172
Sanya Kumari <55986045+sanya2508@users.noreply.github.com>
2020-03-19 12:04:53 +0530

    basic datatypes

---
+++ basic datatypes.sql
@@ -0,0 +1,7 @@
+
+INT   whole numbers
+DECIMAL (M,N)  decimal numbers exact value
+VARCHAR(l)   String of text of length l
+BLOB  BINARY LARGE OBJECT< STORES LARGE DATA
+DATE YYYY-MM-DD
+TIMESTAMP  YYYY-MM-DD HH:MM:SS
